4340364f909228c879b0964545fe7a2210870d6a
maisaengineering <contact@maisasolutions.com>
2013-12-28 13:28:24 +0530

    Upender: unicorn with nginx + asset pipeline fixes

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ end
 
 gem 'jquery-rails'
 
+gem 'unicorn'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
+//= require html5
+//= require tinynav
+//= require jquery.simplemodal
+//= require basic
+
